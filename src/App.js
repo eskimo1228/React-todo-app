016f016c5a7d6c9a803d1963c820036815f11248
eskimo1228 <eskimo1228@outlook.com>
2020-05-07 22:53:26 +0300

    update all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+function Todo() {
+
+  const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+  // return (
+  //   <div
+  //     className="todo"
+  //     style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+  //   >
+  //     {todo.text}
+
+  //     <div>
+  //       <button onClick={() => completeTodo(index)}>Complete</button>
+  //       <button onClick={() => removeTodo(index)}>x</button>
+  //     </div>
+  //   </div>
+  // );
+}
+
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  // we'll render our todos here ...
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
